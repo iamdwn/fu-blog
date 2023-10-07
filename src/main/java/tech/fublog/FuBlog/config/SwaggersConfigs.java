5e2200629fffb62e292840d51eff19e49cdefba5
duogzz <109296329+duogzz@users.noreply.github.com>
2023-10-07 13:30:24 +0700

    merge into main

---
+++ src/main/java/tech/fublog/FuBlog/config/SwaggersConfigs.java
@@ -0,0 +1,31 @@
+//package tech.fublog.FuBlog.config;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@EnableWebMvc
+//@EnableSwagger2
+//@Component
+//public class SwaggersConfigs {
+//}
+//
+//
+//
