781475cab67d345218f75c9502869fac6498a7a8
duogzz <109296329+duogzz@users.noreply.github.com>
2023-10-11 02:00:19 +0700

    update getPopularBlog

--- src/main/java/tech/fublog/FuBlog/controller/BlogPostController.java
+++ src/main/java/tech/fublog/FuBlog/controller/BlogPostController.java
@@ -116,6 +116,17 @@ public class BlogPostController {
         }
     }
 
+    @GetMapping("/getPopularBlogPost")
+    public ResponseEntity<ResponseObject> getPopularBlog() {
+        try {
+            List<BlogPostDTO> blogPostEntity = blogPostService.getPopularBlogPost();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseObject("ok", "post found", blogPostEntity));
+        } catch (BlogPostException ex) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseObject("failed", ex.getMessage(), ""));
+        }
+    }
 
     @GetMapping("getAllBlog/{page}/{size}")
     public PaginationResponseDTO getAllBlog(@PathVariable int page, @PathVariable int size) {
