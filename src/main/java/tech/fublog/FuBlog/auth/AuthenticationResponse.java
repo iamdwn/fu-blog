71a11cc6de5f0eda90bdfd68849f982edf50ef4d
duogzz <109296329+duogzz@users.noreply.github.com>
2023-10-28 15:09:05 +0700

    update setRole

--- src/main/java/tech/fublog/FuBlog/auth/AuthenticationResponse.java
+++ src/main/java/tech/fublog/FuBlog/auth/AuthenticationResponse.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class AuthenticationReponse {
+public class AuthenticationResponse {
 
     private String token;
     private String refreshToken;
