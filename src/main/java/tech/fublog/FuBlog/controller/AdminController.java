df2c4def2a6c92d47b65038488e677620e159d7f
duogzz <109296329+duogzz@users.noreply.github.com>
2023-11-13 04:18:43 +0700

    update countcountBlogByCategory

---
+++ src/main/java/tech/fublog/FuBlog/controller/AdminController.java
@@ -0,0 +1,37 @@
+package tech.fublog.FuBlog.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.fublog.FuBlog.Utility.TokenChecker;
+import tech.fublog.FuBlog.model.ResponseObject;
+import tech.fublog.FuBlog.service.AdminService;
+import tech.fublog.FuBlog.service.BlogPostService;
+
+@RestController
+@RequestMapping("/api/v1/auth/admin/")
+@CrossOrigin(origins = {"http://localhost:5173", "https://fublog.tech"})
+public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/countBlogByCategory/{categoryId}")
+    public ResponseEntity<ResponseObject> countBlogByCategory(@RequestHeader("Authorization") String token,
+                                                              @PathVariable Long categoryId) {
+        try {
+            if (TokenChecker.checkRole(token, false)) {
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(new ResponseObject("ok", "found", adminService.countBlogByCategory(categoryId)));
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseObject("failed", "not found", ""));
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseObject("failed", ex.getMessage(), ""));
+        }
+    }
+
+}
