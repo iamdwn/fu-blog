b187115fd6dfd6d6f1c3bf45d1b79b7b7d026291
duogzz <109296329+duogzz@users.noreply.github.com>
2023-11-13 15:22:36 +0700

    update countBlogByCategory from dung

--- src/main/java/tech/fublog/FuBlog/Utility/DTOConverter.java
+++ src/main/java/tech/fublog/FuBlog/Utility/DTOConverter.java
@@ -91,8 +91,6 @@ public class DTOConverter {
                     .collect(Collectors.toSet());
 
             UserInfoResponseDTO userDTO = convertUserDTO(userEntity);
-            blogPostRepository.save(blogPostEntity);
-
             BlogPostDTO blogPostDTO = new BlogPostDTO(blogPostEntity.getId(),
                     blogPostEntity.getTypePost(),
                     blogPostEntity.getTitle(),
